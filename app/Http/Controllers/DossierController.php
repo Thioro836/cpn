d348473620b5f2c82688f6111fd26648119c3812
bintabah <hadjasorybintabah@gmail.com>
2022-04-30 15:16:47 +0100

    gestionConsultation6

--- app/Http/Controllers/DossierController.php
+++ app/Http/Controllers/DossierController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\{DossierPatient,Patient};
+use Carbon\Carbon;
 class DossierController extends Controller
 {
     /**
@@ -38,6 +39,8 @@ class DossierController extends Controller
      */
     public function store(Request $request)
     {
+        $accouchement = $this->dateAccouchement($request->date_derniere_regle, $request->dure_cycle);
+        $num = $this->genererNumeroDossier();
         DossierPatient::create([
         'date_derniere_regle'=>$request->date_derniere_regle,
         'dure_cycle'=>$request->dure_cycle,
@@ -47,9 +50,27 @@ class DossierController extends Controller
         'taille_patiente'=>$request->taille_patiente,
         'dap'=>$request->dap,
          'id_patient'=>$request->patient,
-         'numero_dossier'=>'001'
+         'numero_dossier'=>$num,
+         'date_accouchement'=>$accouchement
         ]);
         return back()->with('message',"enregistrement reussi");
+    }
+    function genererNumeroDossier(){
+        $liste ='0123456789ABCD';
+        $resultat = "";
+        for($i=1;$i<=4;$i++){
+            $resultat .=substr($liste,(rand()%(strlen($liste))),1); 
+        }
+        return $resultat;
+    }
+    function dateAccouchement($date_derniere_regle, $dure_cycle){
+        $tableau = explode("-",$date_derniere_regle);
+        $jour = $tableau[2];
+        $date = Carbon::parse($date_derniere_regle)->add(9,'month')->format('Y-m-d');
+        $date = explode("-",$date);
+        $date[2]='01';
+        $date = Carbon::parse(collect($date)->join('-'))->format('Y-m-d');
+        return $date;
     }
 
     /**
--- resources/views/formulaires/antecedent/categorie/create.blade.php
+++ resources/views/formulaires/antecedent/categorie/create.blade.php
@@ -1,22 +1,6 @@
 @extends('layouts.master')
 @section('content')
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card-box">
-                @if (\Session::has('message'))
-                    <h4 class="alert alert-success">{{ Session::get('message') }}</h4>
-                @endif
-                <form action="{{ route('categorie-ancetedent.store') }}" method="post">
-                    @csrf
-                    <div class="form-group">
-                        <label for="nom">Nom de la categorie</label>
-                        <input type="text" name="nom" class="form-control" id=nom>
-                    </div>
-                    <button class="btn btn-success " type="submit">Enregistrer</button>
-                </form>
-            </div>
-        </div>
-    </div>
+  
 <div class="row">
     <div class="col-md-6" >
         <div class="card-box">
