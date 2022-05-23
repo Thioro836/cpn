824f0c96eb5c5d22d42006010b67d40a717a630b
bintabah <hadjasorybintabah@gmail.com>
2022-05-23 14:31:59 +0100

    gest1

---
+++ resources/views/formulaires/gestation/table.blade.php
@@ -0,0 +1,34 @@
+<div class="card-box mt-2">
+    <h4 class="header-title">gestations antérieures</h4>
+    <p class="sub-header">
+        For basic styling—light padding and only horizontal dividers—add the base class <code>.table</code> to any <code>&lt;table&gt;</code>.
+    </p>
+
+<div class="table-responsive">
+    <table class="table mb-0">
+        
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Nom</th>
+            <th class="text-right">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+           @foreach ($gestations as $key => $gestation)
+           <tr>
+            <th scope="row">{{ $key+1 }}</th>
+            <td>{{ $gestation->nom_gestation }}</td>
+            <td class="text-right">
+                <a href="{{ route('gestations.edit', $gestation->id_gestation) }}" class="btn btn-primary mt-3">
+                    modifier
+                </a>
+                <a href="{{ route('gestations.destroy', $gestation->id_gestation) }}" class="btn btn-delete btn-danger mt-3">
+                    supprimer
+                </a>
+            </td>
+        </tr>
+           @endforeach
+        </tbody>
+    </table>
+    </div>
